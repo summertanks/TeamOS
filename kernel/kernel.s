23ff4aecd8e14bf70cd9bc0ae9be9393f5fc1c82
Harkirat S Virk <harkiratsvirk@gmail.com>
2022-05-23 13:59:39 -0400

    cleaned up vga.s

--- kernel/kernel.s
+++ kernel/kernel.s
@@ -1,3 +1,28 @@
+; Team OS
+; MIT License
+
+; Copyright (c) 2022 Harkirat Singh Virk
+
+; Permission is hereby granted, free of charge, to any person obtaining a copy
+; of this software and associated documentation files (the "Software"), to deal
+; in the Software without restriction, including without limitation the rights
+; to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+; copies of the Software, and to permit persons to whom the Software is
+; furnished to do so, subject to the following conditions:
+
+; The above copyright notice and this permission notice shall be included in all
+; copies or substantial portions of the Software.
+
+; THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+; IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+; FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+; AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+; LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+; OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+; SOFTWARE.
+
+	%include "include/vga.inc" 
+
 [BITS 32]
 
 extern	init_vga
@@ -6,7 +31,6 @@ extern	terminal_write_string
 global	_kernel_start
 
 _kernel_start:
-	call init_vga
 	mov esi, os_description
 	call terminal_write_string
 loop:
@@ -15,5 +39,37 @@ loop:
 ;------------------------------------------------------------
 section .data
 
-os_description db "Starting TeamOS", 0xA, 0 ; 0xA = line feed
-
+os_description 	db "Starting TeamOS", 0xA, 	; 0xA = line feed
+		db "32 Bit Kernel from Scratch in assembly for training", 0xA,
+		db "32 Bit Kernel from Scratch in assembly for training", 0xA,
+		db "32 Bit Kernel from Scratch in assembly for training", 0xA,
+		db "32 Bit Kernel from Scratch in assembly for training", 0xA,
+		db "32 Bit Kernel from Scratch in assembly for training", 0xA,
+		db "32 Bit Kernel from Scratch in assembly for training", 0xA,
+		db "32 Bit Kernel from Scratch in assembly for training", 0xA,
+		db "32 Bit Kernel from Scratch in assembly for training", 0xA,
+		db "32 Bit Kernel from Scratch in assembly for training", 0xA,
+		db "32 Bit Kernel from Scratch in assembly for training", 0xA,
+		db "32 Bit Kernel from Scratch in assembly for training", 0xA,
+		db "32 Bit Kernel from Scratch in assembly for training", 0xA,
+		db "32 Bit Kernel from Scratch in assembly for training", 0xA,
+		db "32 Bit Kernel from Scratch in assembly for training", 0xA,
+		db "32 Bit Kernel from Scratch in assembly for training", 0xA,
+		db "32 Bit Kernel from Scratch in assembly for training", 0xA,
+		db "32 Bit Kernel from Scratch in assembly for training", 0xA,
+		db "32 Bit Kernel from Scratch in assembly for training", 0xA,
+		db "32 Bit Kernel from Scratch in assembly for training", 0xA,
+		db "32 Bit Kernel from Scratch in assembly for training", 0xA,
+		db "32 Bit Kernel from Scratch in assembly for training", 0xA,
+		db "32 Bit Kernel from Scratch in assembly for training", 0xA,
+		db "32 Bit Kernel from Scratch in assembly for training", 
+		db "32 Bit Kernel from Scratch in assembly for training", 
+		db "32 Bit Kernel from Scratch in assembly for training", 
+		db "32 Bit Kernel from Scratch in assembly for training",
+		db "32 Bit Kernel from Scratch in assembly for training", 
+		db "32 Bit Kernel from Scratch in assembly for training", 
+		db "32 Bit Kernel from Scratch in assembly for training", 
+		db "32 Bit Kernel from Scratch in assembly for training", 
+		db "32 Bit Kernel from Scratch in assembly for training", 
+		db "32 Bit Kernel from Scratch in assembly for training", 0xA
+		db "Copyright (c) Harkirat S Virk", 0xA, 0
--- kernel/vga.s
+++ kernel/vga.s
@@ -1,185 +1,201 @@
+; Team OS
+; MIT License
+
+; Copyright (c) 2022 Harkirat Singh Virk
+
+; Permission is hereby granted, free of charge, to any person obtaining a copy
+; of this software and associated documentation files (the "Software"), to deal
+; in the Software without restriction, including without limitation the rights
+; to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+; copies of the Software, and to permit persons to whom the Software is
+; furnished to do so, subject to the following conditions:
+
+; The above copyright notice and this permission notice shall be included in all
+; copies or substantial portions of the Software.
+
+; THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+; IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+; FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+; AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+; LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+; OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+; SOFTWARE.
+
+	%include "include/vga.inc" 
+
 [BITS 32]
 
-VGA_WIDTH 	equ 	80
-VGA_HEIGHT 	equ 	25
- 
-VGA_COLOR_BLACK		equ 	0
-VGA_COLOR_BLUE		equ 	1
-VGA_COLOR_GREEN		equ 	2
-VGA_COLOR_CYAN		equ 	3
-VGA_COLOR_RED		equ 	4
-VGA_COLOR_MAGENTA	equ 	5
-VGA_COLOR_BROWN		equ 	6
-VGA_COLOR_LIGHT_GREY	equ 	7
-VGA_COLOR_DARK_GREY	equ 	8
-VGA_COLOR_LIGHT_BLUE	equ 	9
-VGA_COLOR_LIGHT_GREEN	equ	10
-VGA_COLOR_LIGHT_CYAN	equ	11
-VGA_COLOR_LIGHT_RED	equ 	12
-VGA_COLOR_LIGHT_MAGENTA	equ 	13
-VGA_COLOR_LIGHT_BROWN	equ 	14
-VGA_COLOR_WHITE		equ	15
-
-VGA_BUFFER		equ	0xB8000
- 
-global	init_vga
+global	terminal_set_color
+global	terminal_get_color
 global	terminal_write_string
 
-; Initialising defaults
-; just initialise the colors
-init_vga:
-	push dx
-	mov dh, VGA_COLOR_WHITE
-	mov dl, VGA_COLOR_BLACK
-	call terminal_set_color
-	pop dx
-	ret
+section .text
 
-; Setting default color
-; IN = dl: bg color, dh: fg color
-; OUT = none
+; Set default color
+; IN al -> 8 bit : foreground 4 bits | background 4 bits
 terminal_set_color:
-	shl dl, 4	; shift left 4 bit
-	or dl, dh	; or with dh
-	; dl -> 8 bit : foreground 4 bits | background 4 bits
-	; saving 
-	mov [terminal_color], dl
+	; saving to default
+	mov [terminal_color], al
 	ret
 
+; Get default color
+; OUT al -> 8 bit : foreground 4 bits | background 4 bits
+terminal_get_color:
+	; return current color
+	mov al, [terminal_color]
+	ret	
 
-; Calculate offset based on cursor (x,y)
-; IN = dl: y, dh: x
-; OUT = dx: Index with offset 0xB8000 at VGA buffer
-; TODO: sanity check on values
+; Write string to terminal
+; IN = ESI: string location
+; OUT = ECX: string size
+terminal_write_string:
+	pusha
+	xor ecx, ecx
 
-terminal_getoffset:
-	; save eax
+	; Print String 
+.write:
+	mov al, [esi]
+
+	; Check for end of string
+	cmp al, 0
+	jz .write_done
+
+	; check for linefeed
+	cmp al, 0xa
+	jne .write_putchar
+	call terminal_putchar.linefeed
+	jmp .write_next
+
+.write_putchar:
+	call terminal_putchar
+
+.write_next:
+	inc esi
+	inc ecx
+	jmp .write
+
+.write_done:
+	; TODO : modify ecx in stack
+	popa
+	ret
+
+; Puts character at current cursor position
+; IN = al: ASCII char
+terminal_putchar:
+	call .get_offset	; return screen offset in dx	
+
+ 	; default color
+	mov ah, [terminal_color]
+	; character to first byte
+	mov byte [VGA_BUFFER + edx], al
+	; set color on second byte
+	mov byte [VGA_BUFFER + edx + 1], ah
+
+	call .inc_offset
+	ret
+
+.get_offset:
 	push eax
 
+	xor eax, eax	; simpler to work with
+	xor edx, edx
+
+	; load cursor - terminal_column at DH
+	;		terminal_row at DL
+	mov dx, [terminal_cursor_pos]
+
+	; Sanity check - bounds assume 80*25
 	; offset = (y * VGA_WIDTH * 2) + (x * 2)
 	; one buffer entry is 2 bytes (dw)
 	shl dh, 1	;	x = x*2
  	shl dl, 1	;	y = y*2
 
-	mov eax, VGA_WIDTH	; upper bits are 0
+	mov al, VGA_WIDTH	; upper bits are 0
+	
 	; y * VGA_WIDTH
-	mul dl			; eax = dl * eax 
-	shr dx, 8		; shift right to dl
-	add dx, ax		; 
+	mul dl			; eax = dl * eax => y * VGA_WIDTH * 2
+	mov dl, dh
+	xor dh, dh		; x => dl
+	add dx, ax		; added offset x 
  
 	pop eax
 	ret
 
-; Put chartacter at cursor value (x,y)
-; IN = dl: y, dh: x, al: ASCII char
-; OUT = none
-terminal_putchar_xy:
-	pusha		; push all
-	
-	; get the offset location
-	call terminal_getoffset
-	mov ebx, edx
-
- 	; default color
-	mov dl, [terminal_color]
-	; character to first byte
-	mov byte [VGA_BUFFER + ebx], al
-	; set color on second byte
-	mov byte [VGA_BUFFER + ebx + 1], dl
- 
-	popa
-	ret
- 
-; put character at current cursor position
-; IN = al: ASCII char
-terminal_putchar:
-	; load cursor - terminal_column at DH, and terminal_row at DL
+; increment cursor position by one, incase end of line, wrap to next
+.inc_offset:
+	; load cursor - terminal_column at DH
+	;		terminal_row at DL
+	xor edx, edx
 	mov dx, [terminal_cursor_pos]
- 	; print character
-	call terminal_putchar_xy
-
+	
 	; adjust cursor position
 	inc dh
-	cmp dh, VGA_WIDTH
-	jne .cursor_moved
+	cmp dh, VGA_WIDTH	
+	jl .save_cursor
  	
-	; wrap arround
+	; inc reached end of the line, wrap around
 	mov dh, 0
 	inc dl
- 
+
+.check_screen_end:
 	; end of screen
 	cmp dl, VGA_HEIGHT
-	jne .cursor_moved
+	jl .save_cursor
  	
-	mov dl, 0
+	; TODO: shift screen up, dont wrap
+	; set to start of the screen
+
+.shift_screen:
+	; slide screen up shift everything left in memory
+	push esi
+	push ecx
+	push eax
+
+	mov edi, VGA_BUFFER
+	mov esi, edi
+	add esi, VGA_WIDTH * 2	; each character takes 2 bytes
+
+	mov ecx, (VGA_HEIGHT - 1) * VGA_WIDTH
+	repz movsw
+
+	mov ecx, VGA_WIDTH
+	xor eax, eax
+	repz stosw
+
+	pop eax
+	pop ecx
+	pop esi
+
+	dec dl
  
-.cursor_moved:
+.save_cursor:
 	; Store new cursor position 
 	mov [terminal_cursor_pos], dx
 	ret
 
-; Print String 
-; IN = cx: length of string, ESI: string location
-; OUT = none
-terminal_write:
-	pusha
-
-.loop:
-	mov al, [esi]
-	call terminal_putchar
- 
-	; decrease counter
-	dec cx
-	cmp cx, 0
-	je .done
- 
-	inc esi
-	jmp .loop
-
-.done:
-	popa
-	ret
-
-; Calculate String length
-; IN = ESI: zero delimited string location
-; OUT = ECX: length of string
-terminal_strlen:
-	push eax
-	push esi
-	mov ecx, 0
-
-.loop:
-	mov al, [esi]
-	cmp al, 0
-	je .done
- 
-	inc esi
-	inc ecx
-	jmp .loop
- 
- .done:
- 	pop esi
-	pop eax
- 	ret
-
-; Write string to terminal
-; IN = ESI: string location
-; OUT = none
-terminal_write_string:
-	pusha
-	; calculate the string length
-	call terminal_strlen
-	; print string
-	call terminal_write
-	popa
-	ret
-
-
+; Go to start of next line
+.linefeed:
+	; load cursor - terminal_column at DH
+	;		terminal_row at DL
+	xor edx, edx
+	mov dx, [terminal_cursor_pos]
+	; start of line
+	mov dh, 0
+	; move to next line
+	inc dl
+	
+	jmp .check_screen_end
+	
 ;-------------------------------------------------------
 
-section .bss 
+section .data
+
 ; default color  
-	terminal_color	resb 	1
+	terminal_color	db 	((VGA_COLOR_BLACK << 4) | VGA_COLOR_WHITE)
+
+;------------------------------------------------------
+
+section .bss
 
 ; current cursor position 
 terminal_cursor_pos:
